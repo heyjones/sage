b4ff42bbb2cb89f66519e069b2940743fa5403f1
Chris Jones <chrisjo@zillow.com>
2017-01-19 11:08:26 -0800

    removed all sage templating logic

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php get_header(); ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
+<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
+<h1><?php the_title(); ?>!</h1>
+<?php the_content(); ?>
 
-<?php the_posts_navigation(); ?>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
--- lib/assets.php
+++ lib/assets.php
@@ -1,37 +1,29 @@
 <?php
 
-namespace Roots\Sage\Assets;
-
-/**
- * Get paths for assets
- */
 class JsonManifest {
   private $manifest;
-
-  public function __construct($manifest_path) {
-    if (file_exists($manifest_path)) {
-      $this->manifest = json_decode(file_get_contents($manifest_path), true);
-    } else {
+  public function __construct($manifest_path){
+    if( file_exists( $manifest_path ) ){
+      $this->manifest = json_decode( file_get_contents( $manifest_path ), true );
+    }else{
       $this->manifest = [];
     }
   }
-
-  public function get() {
+  public function get(){
     return $this->manifest;
   }
-
-  public function getPath($key = '', $default = null) {
+  public function getPath( $key = '', $default = null ){
     $collection = $this->manifest;
-    if (is_null($key)) {
+    if( is_null( $key ) ){
       return $collection;
     }
-    if (isset($collection[$key])) {
+    if( isset( $collection[$key] ) ){
       return $collection[$key];
     }
-    foreach (explode('.', $key) as $segment) {
-      if (!isset($collection[$segment])) {
+    foreach( explode( '.', $key ) as $segment ){
+      if( !isset( $collection[$segment] ) ){
         return $default;
-      } else {
+      }else{
         $collection = $collection[$segment];
       }
     }
@@ -39,20 +31,24 @@ class JsonManifest {
   }
 }
 
-function asset_path($filename) {
+function asset_path( $filename ){
   $dist_path = get_template_directory_uri() . '/dist/';
-  $directory = dirname($filename) . '/';
-  $file = basename($filename);
+  $directory = dirname( $filename ) . '/';
+  $file = basename( $filename );
   static $manifest;
-
-  if (empty($manifest)) {
+  if( empty( $manifest ) ){
     $manifest_path = get_template_directory() . '/dist/' . 'assets.json';
-    $manifest = new JsonManifest($manifest_path);
+    $manifest = new JsonManifest( $manifest_path );
   }
-
-  if (array_key_exists($file, $manifest->get())) {
+  if( array_key_exists( $file, $manifest->get() ) ){
     return $dist_path . $directory . $manifest->get()[$file];
-  } else {
+  }else{
     return $dist_path . $directory . $file;
   }
 }
+
+function assets(){
+  wp_enqueue_style( 'sage/css', asset_path( 'styles/main.css' ), false, null );
+  wp_enqueue_script( 'sage/js', asset_path( 'scripts/main.js' ), ['jquery'], null, true );
+}
+add_action( 'wp_enqueue_scripts', 'assets', 100 );
